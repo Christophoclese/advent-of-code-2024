efa1c97cad14cb6a3e104c2a5d197c48afaef784
Christophoclese <57161+Christophoclese@users.noreply.github.com>
2024-12-03 22:24:19 -0800

    Day 4
    Part 1 done

---
+++ 4/solution.ps1
@@ -0,0 +1,111 @@
+[CmdletBinding()]
+Param(
+    $InputFile = "./input"
+)
+
+If (Test-Path -Path $InputFile -PathType Leaf) {
+    $PuzzleInput = Get-Content -Path $InputFile
+}
+Else {
+    Throw "Invalid input file: $InputFile"
+}
+
+$Width = $PuzzleInput[0].Length
+$Height = $PuzzleInput.Length
+$XMASCount = 0
+$DoubleMASCount = 0
+
+# Start scanning left right, top bottom
+For ($y = 0; $y -lt $Height; $y++) {
+    For ($x = 0; $x -lt $Width; $x++) {
+        # Is this character an X?
+        If ($PuzzleInput[$y][$x] -eq "X") {
+            # Try to find XMAS in all directions...
+
+            # To the right!
+            If ($x + 3 -lt $Width) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y][$x + $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Down and to the right!
+            If ($x + 3 -lt $Width -and $y + 3 -lt $Height) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y + $off][$x + $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Down!
+            If ($y + 3 -lt $Height) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y + $off][$x]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Down and Left!
+            If ($x - 3 -ge 0 -and $y + 3 -lt $Height) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y + $off][$x - $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Left!
+            If ($x - 3 -ge 0) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y][$x - $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Left and Up!
+            If ($x - 3 -ge 0 -and $y - 3 -ge 0) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y - $off][$x - $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Up, Up, and Away!
+            If ($y - 3 -ge 0) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y - $off][$x]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+            # Up and Right!
+            If ($x + 3 -lt $Width -and $y - 3 -ge 0) {
+                $String = ""
+                For ($off = 0; $off -lt 4; $off++) {
+                    $String += $PuzzleInput[$y - $off][$x + $off]
+                }
+                If ($String -eq "XMAS") { $XMASCount++ }
+            }
+
+        }
+
+        # Is the current character an A?
+        If ($PuzzleInput[$y][$x] -eq "A") {
+            # Not on an edge in any direction
+            If ($x + 1 -lt $Width -and $x -1 -ge 0 -and $y + 1 -lt $Height -and $y - 1 -ge 0) {
+                
+            }
+        }
+
+    }
+}
+
+Write-Host "The total number of XMAS appearances is: $XMASCount"
+Write-Host "The total number of X-MAS appearances is: $DoubleMASCount"
