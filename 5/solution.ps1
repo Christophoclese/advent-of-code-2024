7c758994d0765067b6ee6adff8144b238fc01961
Christophoclese <57161+Christophoclese@users.noreply.github.com>
2024-12-04 22:52:50 -0800

    Day 5 Part 2 - incomplete
    Part 1 still works...maybe getting close?

--- 5/solution.ps1
+++ 5/solution.ps1
@@ -10,41 +10,71 @@ Else {
     Throw "Invalid input file: $InputFile"
 }
 
-$Ruleset = $PuzzleInput[0].split("`r`n") | Where-Object { $_ }
-$Updates = $PuzzleInput[1].split("`r`n") | Where-Object { $_ }
-$MiddlePageSum = 0
+function CheckRules($Values, $LastChance) {
+    $ValidUpdate = $false
 
-ForEach ($Update in $Updates) {
-    $ValidUpdate = $true
-    $Parts = $Update.split(",") 
     # See if each rule applies to the numbers in this update?
     ForEach ($Rule in $Ruleset) {
-        Write-Verbose "Evaluating $Rule on $Update"
+        Write-Verbose "Evaluating $Rule on $($Values -join(','))"
         $Before, $After = $Rule.split("|")
-        If ($Before -in $Parts -and $After -in $Parts) {
+        If ($Before -in $Values -and $After -in $Values) {
             # Enforce the rule
-            $BeforeIndex = $Parts.IndexOf($Before)
-            $AfterIndex = $Parts.IndexOf($After)
+            $BeforeIndex = $Values.IndexOf($Before)
+            $AfterIndex = $Values.IndexOf($After)
             If ($BeforeIndex -gt $AfterIndex) {
-                Write-Verbose "Rule $Rule is not valid because $Before ($BeforeIndex) appears after $After ($AfterIndex)"
-                $ValidUpdate = $false
+                Write-Verbose "FAILED: Rule $Rule is not valid because $Before ($BeforeIndex) appears after $After ($AfterIndex)"
+
+                If (-not $LastChance) {
+                    # We'll attempt to swap the positions of Before and After, then validate again?
+                    $SwappedParts = $Parts.Clone()
+                    $SwappedParts[$BeforeIndex] = $After
+                    $SwappedParts[$AfterIndex] = $Before
+                    Write-Verbose "REPAIRING: Rule $Rule"
+                    return CheckRules -Values $SwappedParts -LastChance $true
+                }
+
+                return $false
             }
             Else {
-                Write-Verbose "Rule $Rule is valid for $Update"
+                Write-Verbose "SUCCESS: Rule $Rule is valid for $($Values -join(','))"
+                $ValidUpdate = $true
             }
         }
         Else {
-            Write-Verbose "Rule $Rule does not apply for $Update"
+            Write-Verbose "IGNORED: Rule $Rule does not apply for $($Values -join(','))"
+            $ValidUpdate = $true
         }
     }
+
+    Return $ValidUpdate
+}
+
+$Ruleset = $PuzzleInput[0].split("`r`n") | Where-Object { $_ }
+$Updates = $PuzzleInput[1].split("`r`n") | Where-Object { $_ }
+$CorrectMiddlePageSum = 0
+
+ForEach ($Update in $Updates) {
+    $Parts = $Update.split(",") 
+
+    $ValidUpdate = CheckRules -Values $Parts -LastChance $true
 
     If ($ValidUpdate) {
         Write-Verbose "$Update - Appears Valid"
         # All the rules passed
         # Find middle value and add to sum
         $Mid = [int][Math]::Truncate($Parts.Count / 2)
-        $MiddlePageSum += [int]$Parts[$Mid]
+        $CorrectMiddlePageSum += [int]$Parts[$Mid]
+    }
+    Else {
+        Write-Verbose "$Update - Is Invalid...can we fix it?"
+        If (CheckRules -Values $Parts -LastChance $false) {
+            Write-Verbose "FIXED!?!?!"
+        }
+
     }
 }
 
-Write-Host "The sum of the middle page of correctly ordered updates is: $MiddlePageSum"
+Write-Host "The sum of the middle page of correctly ordered updates is: $CorrectMiddlePageSum"
+
+
+# TODO: Consider returning the matching parts/swapped parts and using that to test for true/false, then can extract middle value from used parts
